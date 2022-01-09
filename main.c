c11f76920e97299cd1aa9c34a8dbe84f46f65773
Alexandre <alexandre-kevin.de-freitas-martins@epitehc.eu>
2022-01-09 23:47:53 +0100

    Fix -h

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ void print_manual(void)
                 "jump and crouch to avoid the ennemies on the screen in.\n");
     my_putstr("The dino will move faster with the score.\n");
     my_putstr("\n                                                  RULES\n\n");
-    my_putstr("Don't be killed by ennemies.\n");
+    my_putstr("Don't be killed by ennemies. (You must avoid ennemies)\n");
     my_putstr("\n                                                  "
                 "CONTROLS\n\n");
     my_putstr("      - Click on the play bouton to start or exit bouton to "
@@ -30,24 +30,31 @@ void print_manual(void)
     my_putstr("      - Once in game press escape to pause.\n");
     my_putstr("      - If paused press space or click on the play bouton to "
                 "resume, you can also click on the exit bouton to exit.\n");
-    my_putstr("      - If you Game Over you can press R to retry or Escape "
+    my_putstr("      - If you Game Over you can press Retry or Escape bouton"
     "to close.\n");
 }
 
 void print_manual2(void)
 {
-    my_putstr("      - Jump with RSHIFT and Crouch with LShift.\n\n");
+    my_putstr("      - Jump with RSHIFT and Crouch with LShift.\n");
+    my_putstr("\n                                                  "
+                "MAPS\n\n");
+    my_putstr("  Replace empty spaces with numbers of your choice\n\n");
+    my_putstr("      - 1 = basic snowman\n");
+    my_putstr("      - 2 = flying snowman\n");
+    my_putstr("      - 4 = coin\n");
+    my_putstr("      - 4 = flag to finish\n\n");
 }
 
 int main(int argc, char **argv)
 {
-    if (argc == 2) {
-        srand(time(0));
-        display_window(argv[1]);
-    } else if (argc == 2 && my_strlen(argv[1]) == 2 && argv[1][0] == '-'
+    if (argc == 2 && my_strlen(argv[1]) == 2 && argv[1][0] == '-'
     && argv[1][1] == 'h') {
         print_manual();
         print_manual2();
+    } else if (argc == 2) {
+        srand(time(0));
+        display_window(argv[1]);
     }
     return (0);
 }
